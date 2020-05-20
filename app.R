d33eaa00878c3391b8ebb119ba0985fb3489a0bc
Eliyahou <eliyahou.gabbay@mines-ales.org>
2020-05-21 00:31:31 +0200

    Data processed & leaflet visualisation

--- app.R
+++ app.R
@@ -12,41 +12,142 @@
 
 
 
-# load required packages
+# Load Required Packages
 library(shiny)
 library(leaflet)
 library(geojsonio)
+library(RColorBrewer)
 
-############## Read CSV ###############""
-df = read.csv("COVID_data_2020-05-17.csv")
-worldcountry = geojson_read("countries.geojson", what = "sp")
+
+
+
+############## Read files ###############""
+cv_cases = read.csv("input_data/coronavirus.csv")
+df = read.csv("input_data/COVID_data_2020-05-17.csv")
+worldcountry = geojson_read("input_data/50m.geojson", what = "sp")
+country_geoms = read.csv("input_data/country_geoms.csv")
+countries = read.csv("input_data/countries_codes_and_coordinates.csv")
+
+
+### Data ###
+
+# for the date given
+date = "2020-05-17"
+# df for a specific date
+spec_date_df = df[df$date == date,]
+# order country by df order
+bigOne <- merge(worldcountry, 
+                merge(spec_date_df, countries, by = "country"), 
+                by.x = "ADM0_A3", by.y = "alpha3")
+
+worldcountryData <- worldcountry[match(bigOne$ADM0_A3, worldcountry$ADM0_A3),] %>%
+  cbind(cases = bigOne$cases,
+        newcases = bigOne$new_cases, 
+        deaths = bigOne$deaths,
+        newdeaths = bigOne$new_cases,
+        recovered = bigOne$recovered,
+        newrecovered = bigOne$new_recovered)
+colnames(worldcountryData@data)[c(72, 73, 74, 75, 76, 77)] <- c("cases",
+                                                                "new_cases",
+                                                                "deaths",
+                                                                "new_deaths",
+                                                                "recovered", 
+                                                                "new_recovered")
+
 
 
 
 ###   MAPPING   ###
+palettes <- c("cases" = "Blues", "deaths" = "OrRd", "recovered" = "YlGn")
+
+basemap <- leaflet(worldcountryData) %>%
+                addPolygons(
+                  fillColor = ~ palDeaths(deaths),
+                  fillOpacity = 1,
+                  color = "black",
+                  weight = 1
+                ) %>%
+                setView(lng = 0,
+                        lat = 30,
+                        zoom = 1.4) %>%
+                addLegend(
+                  position = "bottomright",
+                  pal = palDeaths,
+                  values = ~ deaths,
+                  opacity = 1
+                )
 
 
 
 
-u.i <- fluidPage(titlePanel("Covid Mapping"),
-                 
-                 sidebarLayout(position = "left",
-                   
-                   radioButtons(inputId = "state", label =  "Choose a highlighted Map",
-                                choices = c("Cases", 
-                                            "New cases",
-                                            "Deaths", 
-                                            "New Deaths",
-                                            "Recoverd",
-                                            "New Recovered")),
+#### Shiny Application ###
+u.i <- fluidPage(
+  
+  titlePanel("Covid Mapping"),
+  
+  sidebarLayout(position = "left",
+                
+                sidebarPanel(
+                  
+                  radioButtons(
+                    inputId = "category",
+                    label =  "Category",
+                    choices = list(
+                      "Cases" = 1,
+                      "New cases" = 2,
+                      "Deaths" = 3,
+                      "New Deaths" = 4,
+                      "Recoverd" = 5,
+                      "New Recovered" = 6
+                    )
+                  ),
+                  
+                  sliderInput("date",
+                              label = "Date",
+                              min = as.Date("2020-01-22","%Y-%m-%d"),
+                              max = as.Date("2020-05-17", "%Y-%m-%d"),
+                              value = as.Date("2020-05-17", "%Y-%m-%d"),
+                              timeFormat = "%F")
+                ),
+                
+                mainPanel(h3("Leaflet plot maybe one day ?"),
+                          basemap
+                          )
+                )
+  )
 
-                 mainPanel(
-                   h3("Leaflet plot is coming soon.."))
-                 )
-          )
+
+ser.ver <- function(input, output){
+  
+  reactive_db_date = reactive({
+    spec_date_df = df[df$date == input$date,]
+  })
+  
+  reactive_db_category = reactive({
+    if (input$category == "Cases" | input$category == "New Cases"){
+      pal = colorBin(palettes = "Blues", domain = worldcountryData$cases)
+    }
+    if (input$category == "New Cases"){
+      pal = colorBin("Blues", domain = worldcountryData$new_Cases)
+    }
+    if (input$category == "Deaths" | input$category == "New Cases"){
+      pal = colorBin(palettes = "OrRd", domain = worldcountryData$deaths)
+    }
+    if (input$category == "New Deaths"){
+      pal = colorBin("OrRd", domain = worldcountryData$new_deaths)
+    }
+    if (input$category == "Recovered" | input$category == "New Cases"){
+      pal = colorBin(palettes = "YlGn", domain = worldcountryData$recovered)
+    }
+    if (input$category == "New Recovered"){
+      pal = colorBin("YlGn", domain = worldcountryData$new_recovered)
+    }
+  })
+  
+  
+}
 
 
-ser.ver <- function(input, output){}
 
 
 # Define UI for app that draws a histogram ----
@@ -107,3 +208,10 @@ server <- function(input, output) {
 }
 
 shinyApp(ui = u.i, server = ser.ver)
+
+
+
+
+
+
+
